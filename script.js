8696fd848054652f879e9ed1548612814c0a4094
abhishake2323 <abhishakem@hotmail.com>
2024-10-17 22:02:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+let username= null;
+
+
+function loadStorage(){
+username=    window.localStorage.getItem("username");
+document.getElementById("display-name").innerHTML=username;
+}
+function saveUserInputTolocalStorage(){
+    username = document.getElementById("username").value;
+    window.localStorage.setItem("username", username);
+};
+
+
+function clearInputs(){
+
+    document.getElementById("username").value =""
+    window.localStorage.clear();
+
+}
+
+loadStorage()
